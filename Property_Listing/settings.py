5430b94a8dcf381d46924aee0e5d34ab2bd8dbce
pawan-nickelfox <pawankumar131098@gmail.com>
2021-12-27 20:27:10 +0530

    static

--- Property_Listing/settings.py
+++ Property_Listing/settings.py
@@ -147,11 +147,9 @@ MESSAGE_TAGS={
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATIC_ROOT = 'staticfiles'
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
