1e2a9b2ab29c4c47abd85442440d3e3b67827839
pawan-nickelfox <pawankumar131098@gmail.com>
2021-12-28 09:28:34 +0530

    static

--- Property_Listing/settings.py
+++ Property_Listing/settings.py
@@ -134,7 +134,7 @@ LOGOUT_URL ='/home/'
 
 # media folder settings
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [MEDIA_ROOT]
